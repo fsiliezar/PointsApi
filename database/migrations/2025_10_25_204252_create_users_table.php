00fb4e056893e8c842e7b54e724514a37ac0a421
Francisco Siliezar <francisco32siliezar@gmail.com>
2025-10-26 23:16:35 -0600

    se modifica usertable

--- database/migrations/2025_10_25_204252_create_users_table.php
+++ database/migrations/2025_10_25_204252_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->string('email');
             $table->timestamps();
         });
